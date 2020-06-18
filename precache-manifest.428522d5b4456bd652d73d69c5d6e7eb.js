b701a85300c363e9f1fdb3a10572fffaac2c26d3
Takato Cox <takatohockey@gmail.com>
2020-06-18 11:07:50 -0700

    Updates

---
+++ precache-manifest.428522d5b4456bd652d73d69c5d6e7eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e626786b7fbde3268996dd9353e1da8",
+    "url": "/ReactFamilyCards/index.html"
+  },
+  {
+    "revision": "c9a68db47de0b0836ed7",
+    "url": "/ReactFamilyCards/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "2bcb6f102e650c6b9d64",
+    "url": "/ReactFamilyCards/static/css/main.7cf16294.chunk.css"
+  },
+  {
+    "revision": "c9a68db47de0b0836ed7",
+    "url": "/ReactFamilyCards/static/js/2.57e59cc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactFamilyCards/static/js/2.57e59cc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bcb6f102e650c6b9d64",
+    "url": "/ReactFamilyCards/static/js/main.26064aba.chunk.js"
+  },
+  {
+    "revision": "bc8e272ff4d89eb10de0",
+    "url": "/ReactFamilyCards/static/js/runtime-main.041fb6b4.js"
+  }
+]);
